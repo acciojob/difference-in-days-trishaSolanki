51c8647713ae6c626870cc4ad0bd4822b0fbbf68
trishaSolanki <141935315+trishaSolanki@users.noreply.github.com>
2023-11-22 13:01:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,21 @@
 var dateDiffInDays = function (date1, date2) {
   //   write your code here
+	const utcDate1 = Date.UTC(
+    parseInt(date1.substring(0, 4)),
+    parseInt(date1.substring(5, 7)) - 1, // Months are zero-based
+    parseInt(date1.substring(8))
+  );
+	const utcDate2 = Date.UTC(
+		parseInt(date2.subString(0,4)),
+		parseInt(date2.subString(5,7)) - 1,
+		parseInt(date2.subStirng(8))
+	);
+	const timeDiff = utcDate2 - utcDate1;
+
+  // Convert the difference to days
+  const daysDiff = Math.floor(timeDiff / (24 * 60 * 60 * 1000));
+
+  return daysDiff;
 };
 
 // Do not change the code below.
