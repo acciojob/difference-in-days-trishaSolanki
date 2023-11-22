52111944d0fd36d57c65fefdf2c8b6a5cb06368b
trishaSolanki <141935315+trishaSolanki@users.noreply.github.com>
2023-11-22 15:08:50 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,19 +1,18 @@
 var dateDiffInDays = function (date1, date2) {
-  //   write your code here
-	const dateOne = new Date(date1);
-	const DateTwo = new Date(date2);
+  const dateOne = new Date(date1);
+  const dateTwo = new Date(date2);
 
-	const firstDateInMs = firstDate.getTime();
-	const secondDateInMs = secondDate.getTime();
+  const firstDateInMs = dateOne.getTime();
+  const secondDateInMs = dateTwo.getTime();
 
-	const diffMs =  secondDateInMs - firstDateInMs 
+  const diffMs =  secondDateInMs - firstDateInMs 
 
-	const aDaysInMs = 20*60*60*1000
-	const diff = Math.round(diffMs / aDaysInMs);
-	return diff;
+  const aDayInMs = 24*60*60*1000
+  const diff = Math.round(diffMs / aDayInMs);
+  
+  return diff;
 };
 
-// // Do not change the code below.
 const dateOne = prompt("Enter Start Date.");
- const dateTwo = prompt("Enter End Date.");
- alert(dateDiffInDays(dateOne, dateTwo));
+const dateTwo = prompt("Enter End Date.");
+alert(dateDiffInDays(dateOne, dateTwo));
