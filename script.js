36c42e9111be4cba251c42bd7e242aae7614b578
trishaSolanki <141935315+trishaSolanki@users.noreply.github.com>
2023-11-22 14:54:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,24 +1,19 @@
 var dateDiffInDays = function (date1, date2) {
   //   write your code here
-	const DateOne = Date.UTC(
-    parseInt(date1.substring(0, 4)),
-    parseInt(date1.substring(5, 7)) - 1, // Months are zero-based
-    parseInt(date1.substring(8))
-  );
-	const DateTwo = Date.UTC(
-		parseInt(date2.subString(0,4)),
-		parseInt(date2.subString(5,7)) - 1,
-		parseInt(date2.subStirng(8))
-	);
-	const timeDiff = Datetwo - DateOne;
+	const dateOne = new Date("2022-03-01");
+	const DateTwo = new Date();
 
-  // Convert the difference to days
-  const dateDiffInDays = Math.floor(timeDiff / (24 * 60 * 60 * 1000));
+	const firstDateInMs = firstDate.getTime();
+	const secondDateInMs = secondDate.getTime();
 
-  return dateDiffInDays;
+	const diffMs =  secondDateInMs - firstDateInMs 
+
+	const aDaysInMs = 20*60*60*1000
+	const diff = Math.round(diffMs / aDaysInMs);
+	
 };
 
-// Do not change the code below.
+// // Do not change the code below.
 const dateOne = prompt("Enter Start Date.");
-const dateTwo = prompt("Enter End Date.");
-alert(dateDiffInDays(dateOne, dateTwo));
+ const dateTwo = prompt("Enter End Date.");
+ alert(dateDiffInDays(dateOne, dateTwo));
